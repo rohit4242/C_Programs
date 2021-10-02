013e322cd8387ec35308aac676b0b299fa576502
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-02 12:21:49 +0530

    Add files via upload

---
+++ Unit-1/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    
+    int a, b, addition, subtraction, multiplication, division;
+   
+    printf("\t\t** This Calculator Perform All Arithmetic Operations. **");
+    printf("\nEnter Your First Number: ");
+    scanf("%d", &a);
+    printf("\nEnter Your Second Number: ");
+    scanf("%d", &b);
+
+    addition = a+b;
+    subtraction = a-b;
+    multiplication = a * b;
+    division = a/b;
+    
+    printf("\nYour Entered Numbers is: %d  %d\n", a, b);
+
+    printf("\nYour additions is: %d", addition);
+    printf("\nYour subtractions is: %d", subtraction);
+    printf("\nYour multiplications is: %d", multiplication);
+    printf("\nYour divisions is: %d", division);
+
+    printf("\n\n** Thank You For Using This Calculator **");
+
+    return 0;
+}
