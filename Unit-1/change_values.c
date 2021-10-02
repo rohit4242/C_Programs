96701a4d997a172bcd868d83c60548b4f3367f9d
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-02 13:49:03 +0530

    Add files via upload

---
+++ Unit-1/change_values.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    
+    int a, b, c; 
+   
+    printf("\t\t** Interchange The Values. **");
+    
+    printf("\nEnter Your First Number: ");
+    scanf("%d", &a);
+    printf("\nEnter Your Second Number: ");
+    scanf("%d", &b);
+
+    printf("\nBefore Swapping \na = %d\nb = %d", a, b);
+
+    c = a;
+    a = b;
+    b = c;
+    
+    printf("\nAfter Swapping \na = %d\nb = %d", a, b);
+
+    printf("\n\n** Thank You For Using This Calculator **");
+
+    return 0;
+}
