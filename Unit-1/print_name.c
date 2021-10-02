f8c75ac9ff655a77fee75d77f69868ee176cc786
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-02 11:53:53 +0530

    Update and rename helloworld.c to print_name.c

---
+++ Unit-1/print_name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("\n************");
+   printf("\n*Rohit Luni*");
+   printf("\n************");
+
+   return 0;
+}
