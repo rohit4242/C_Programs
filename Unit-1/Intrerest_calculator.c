b6a88ea3e9fd7626842869cff35bde472d53b112
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-02 12:49:45 +0530

    Add files via upload

---
+++ Unit-1/Intrerest_calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    
+    int amount, rate, time_period; 
+    float interest;
+   
+    printf("\t\t** Interest Calculator. **");
+    printf("\nEnter Your Amount: ");
+    scanf("%d", &amount);
+    printf("\nEnter Your Rate: ");
+    scanf("%d", &rate);
+    printf("\nEnter Your Time Period: ");
+    scanf("%d", &time_period);
+
+    interest = amount*rate*time_period/100;
+    
+    printf("\nYour Total Interest is: %f\n", interest);
+
+    printf("\n\n** Thank You For Using This Calculator **");
+
+    return 0;
+}
