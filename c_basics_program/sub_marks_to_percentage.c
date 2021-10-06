b73a0db56dee228b87d90ab75fba1f61bad259a8
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-06 14:43:02 +0530

    Create sub_marks_to_percentage.c

---
+++ c_basics_program/sub_marks_to_percentage.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    
+    int sub_1, sub_2, sub_3, sub_4, sub_5; 
+    float percentage;
+   
+    printf("\t\t** percentage Calculator. **");
+    printf("\nEnter Your First Subject Marks: ");
+    scanf("%d", &sub_1);
+    printf("\nEnter Your Second Subject Marks: ");
+    scanf("%d", &sub_2);
+    printf("\nEnter Your Third Subject Marks: ");
+    scanf("%d", &sub_3);
+    printf("\nEnter Your Forth Subject Marks: ");
+    scanf("%d", &sub_4);
+    printf("\nEnter Your Fifth Subject Marks: ");
+    scanf("%d", &sub_5);
+  
+    printf("\nSum of all Subject Marks: %d", sub_1+sub_2+sub_3+sub_4+sub_5);
+  
+    percentage = (float)(sub_1+sub_2+sub_3+sub_4+sub_5)/5;
+   
+    printf("\nYour Percentage is: %.2f\n", percentage);
+
+    printf("\n\n** Thank You For Using This Calculator **");
+
+    return 0;
+}
