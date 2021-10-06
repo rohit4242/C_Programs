8b5d3f84ca2789069ef23007d0b1ba3cb6468cad
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-06 15:29:03 +0530

    Update sum_of_middel_num.c

--- c_basics_program/sum_of_middel_num.c
+++ c_basics_program/sum_of_middel_num.c
@@ -2,20 +2,20 @@
 
 int main(){
 
-    int a, middel_num, square, num;
+    int a, middle_num, square, num;
 
     printf("\nEnter Your Number: ");
     scanf("%d",&num);
 
     // logic
     a = num/1; // return first two digit from number
-    middel_num = a%10; // return middel number
+    middle_num = a%10; // return middle number
 
-    square = middel_num * middel_num; // square of middel number
+    square = middle_num * middle_num; // square of middle number
 
     printf("\n %d", a);
 
-    printf("\n\nYour Middel Number is: %d", middel_num);
+    printf("\n\nYour Middle Number is: %d", middle_num);
     printf("\nYour Square is: %d", square);
 
     return 0;
