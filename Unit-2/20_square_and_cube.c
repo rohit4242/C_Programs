ac482c21ada56b4c5f52b1cc0dfd8c286f4eebcc
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-10-17 18:51:51 +0530

    Add files via upload

---
+++ Unit-2/20_square_and_cube.c
@@ -0,0 +1,21 @@
+// 20. Print first 10 natural number with its square and cube.
+
+#include <stdio.h>
+
+int main(){
+
+    int i, num;
+
+    printf("\nEnter You Want to Print Number: ");
+    scanf("%d", &num);
+
+    printf("\nNumber\tSquare\tCube");
+    printf("\n======================");
+
+    for (i=1;i<=num;i++){
+
+        printf("\n%d\t%d\t%d",i, i*i, i*i*i);
+        
+    } 
+    return 0;
+}
