fcd690dc9a744cb90ee8f6fbe0d16ec44aada6da
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-11-24 16:37:36 +0530

    Update u3p1.c

--- Unit-3/u3p1.c
+++ Unit-3/u3p1.c
@@ -1,1 +1,22 @@
+/* 1. Write a program to find out addition of two values using UDF.*/
 
+#include <stdio.h>
+
+int sum(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int a, b;
+
+    printf("\nEnter Your First Number: ");
+    scanf("%d",&a);
+    printf("\nEnter Your Second Number: ");
+    scanf("%d",&b);
+    
+    printf("\nYour Sum Is: %d",sum(a, b));
+
+    return 0;
+}
