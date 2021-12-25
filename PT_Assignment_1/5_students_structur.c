00bf5c874aef7ccea4628787398714d4ebdcabcc
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-12-25 15:10:40 +0530

    Add files via upload

---
+++ PT_Assignment_1/5_students_structur.c
@@ -0,0 +1,15 @@
+/*
+Write a menu driven C program which creates student structure with appropriate
+attributes and give the following option in menu.
+1) Add record in File.
+2) Search Record from file based on Roll No
+3) Display All records.
+*/
+#include <stdio.h>
+
+int main()
+{
+
+
+    return 0;
+}
